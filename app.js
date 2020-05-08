7497ad78ab7f8f18e720d2591da8341babc5aff1
youfang <981545521@qq.com>
2020-05-08 13:49:30 +0800

    监听器

--- app.js
+++ app.js
@@ -209,6 +209,39 @@ App({
         if (_this.data[page] && _this.data[page].hasNextPage) {
             callback(_this.data[page].pageNum + 1);
         }
+    },
+    /**
+     *  设置监听器
+     * <pre>
+     *            //调用监听器，监听数据变化
+                 App.watch(this, {
+                        test: function (newVal) {
+                            console.log(newVal)
+                        }
+                    })
+     *</pre>
+     */
+    watch: function (ctx, obj) {
+        Object.keys(obj).forEach(key => {
+            this.observer(ctx.data, key, ctx.data[key], function (value) {
+                obj[key].call(ctx, value)
+            })
+        })
+    },
+    // 监听属性，并执行监听函数
+    observer: function (data, key, val, fn) {
+        Object.defineProperty(data, key, {
+            configurable: true,
+            enumerable: true,
+            get: function () {
+                return val
+            },
+            set: function (newVal) {
+                if (newVal === val) return
+                fn && fn(newVal)
+                val = newVal
+            },
+        })
     }
 
 });
